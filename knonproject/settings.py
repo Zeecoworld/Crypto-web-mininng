a6373623b744ee792517cf193d57f3a904e1f6e8
Isaac <engrisaac1234@gmail.com>
2021-12-12 16:02:24 +0100

    second shit

--- knonproject/settings.py
+++ knonproject/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR , "static"),)
 
 
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR , "static")
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
